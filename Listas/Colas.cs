0e5e83262f852f32cb8b38873449a9577d456213
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2022-05-02 18:18:23 -0700

    termine el codigo pendiente

--- Listas/Colas.cs
+++ Listas/Colas.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace Listas
 {
-    internal class Colas
+    public class Colas
     {
+        private List<string> lista;
+        public Colas()
+        {
+            lista = new List<string>();
+
+        }
+
+        public void Agregar(string dato)
+
+        {
+            lista.Add(dato);
+        }
+
+        private bool ValidaVacio()
+        {
+            return (lista.Count == 0);
+        }
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("Lista Vacia");
+            }
+            lista.RemoveAt(0);
+        }
+
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+
+            if (ValidaVacio())
+            {
+                return "lista vacia";
+            }
+
+            int listCount = lista.Count;
+            for (int i = 0; i < listCount; i++)
+            {
+                if (i > 0)
+                {
+                    datos += "\n";
+                }
+                datos += $"[{i}] - {lista[i]}";
+            }
+            return datos;
+        }
     }
 }
--- Listas/Pilas.cs
+++ Listas/Pilas.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace Listas
 {
-    internal class Pilas
+    public class Pilas
     {
+        private List<string> list;
+        public Pilas()
+        {
+            list = new List<string>();
+        }
+
+        public void Agregar(string dato)
+        {
+            list.Add(dato);
+        }
+
+        private bool ValidaVacio()
+        {
+            return list.Count == 0;
+        }
+
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("Lista Vacia");
+            }
+
+            list.RemoveAt(list.Count - 1);
+        }
+
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+
+            if (ValidaVacio())
+            {
+                return "Lista vacia";
+            }
+
+            int lstCount = list.Count;
+            for (int i = 0; i < lstCount; i++)
+            {
+                if (i > 0)
+                {
+                    datos += "\n";
+                }
+
+                datos += $"[{i}] - {list[i]}";
+            }
+            return datos;
+        }
     }
 }
